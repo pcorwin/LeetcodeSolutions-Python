1179fb51118fae7cd0bf077afb4adb17894dfb89
pcorwin <phoebe.corwin23@gmail.com>
2023-07-09 20:07:18 -0400

    Changing file format to classes to implement basic interface

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-#add interface to either type in the name of the desired file or print by difficulty
-#
-#
-from os.path import dirname, basename, isfile, join
-import glob
+import os
+import importlib.util
 
 
+def run_function(module_path, function_name):
+    spec = importlib.util.spec_from_file_location("", module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")]
+    if hasattr(module, function_name):
+        function = getattr(module, function_name)
+        function()
+    else:
+        print(f"Function '{function_name}' not found in '{module_path}'")
 
-module = 'Reverse_Bits.py'
 
-from Easy import Reverse_Bits as r
+def process_directory(directory, function_name):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".py"):
+                module_path = os.path.join(root, file)
+                run_function(module_path, function_name)
 
-print(module.Solution().solve())
+
+# Replace "directory_path" with the path to your folder containing Python files
+directory_path = "Easy"
+# Replace "your_function_name" with the actual name of the function you want to execute
+function_name = "solve"
+
+process_directory(directory_path, function_name)
